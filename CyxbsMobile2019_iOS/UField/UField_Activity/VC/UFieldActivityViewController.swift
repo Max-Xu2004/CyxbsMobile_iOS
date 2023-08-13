4b979eb374244b82fb01e2b6e722bd59a06d5f8f
Max Xu <114380848+Max-Xu2004@users.noreply.github.com>
2023-08-13 15:41:55 +0800

    [create]建立UFieldActivityViewController

---
+++ CyxbsMobile2019_iOS/UField/UField_Activity/VC/UFieldActivityViewController.swift
@@ -0,0 +1,16 @@
+//
+//  UFieldActivityViewController.swift
+//  CyxbsMobile2019_iOS
+//
+//  Created by 许晋嘉 on 2023/8/13.
+//  Copyright © 2023 Redrock. All rights reserved.
+//
+
+import UIKit
+
+class UFieldActivityViewController: UIViewController {
+    override func viewDidLoad() {
+        <#code#>
+    }
+    
+}
